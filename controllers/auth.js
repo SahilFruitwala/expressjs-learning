f8846c6b8ef904e36dfe768eaddf30e928f77fae
SahilFruitwala <sahilfruitwala007@gmail.com>
2021-02-03 18:40:05 -0400

    Added Mail on Signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,23 @@
+require("dotenv").config();
+
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+const API_KEY = process.env.SENDGRID_API;
+const SENDER = process.env.SENDER_MAIL;
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: API_KEY,
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
-  let message = req.flash('error');
+  let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
   } else {
@@ -83,6 +97,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: SENDER,
+            subject: "Signup Success!",
+            html: "<h1>You successfully signed up!!!</h1>",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => console.log(err));
